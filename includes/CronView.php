3abdedcfb1c38d6f4672709693baebedf8c0585a
root <root@h2323364.stratoserver.net>
2015-03-16 08:19:26 +0100

    timeline ohne login nutzbar gemacht

--- includes/CronView.php
+++ includes/CronView.php
@@ -1,27 +1,5 @@
 <?PHP
 
-if ($_SESSION['config']->CURRENTUSER->STATUS != "admin" && $_SESSION['config']->
-    CURRENTUSER->STATUS != "user") {
-
-    /* ------------------------------------
-    BENUTZERSTATUS ANZEIGEN
-    ------------------------------------ */
-    $USR = $_SESSION['config']->CURRENTUSER;
-
-    $USERSTATUS = new UserStatus($USR, -1, -1);
-
-    $tbl = new Table(array(""));
-    $tbl->setAlign("center");
-    $r = $tbl->createRow();
-    $r->setAttribute(0, $USERSTATUS);
-    $tbl->addRow($r);
-
-    $tbl->show();
-    /* --------------------------------- */
-
-
-} else {
-
 
     $spc = new Spacer(20);
     $ln = new Line();
@@ -65,7 +43,6 @@ if ($_SESSION['config']->CURRENTUSER->STATUS != "admin" && $_SESSION['config']->
   
   
     $scDbTable->show();
-}
 
 ?>
 
