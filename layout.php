a855e20b22669e2238201fddbde6ba0cd3a2e868
root <root@h2323364.stratoserver.net>
2015-08-25 16:11:01 +0200

    jetzt auch wirklich mit Link ;)

--- layout.php
+++ layout.php
@@ -234,8 +234,9 @@ $layoutTable->addRow($fussLayoutRow);
 
 $versionInfo = "Version: " .file_get_contents('version.txt');;
 $lVersion = new Link("http://smarthomeyourself.de/statusInfo.php", $versionInfo);
+
 $versionLayoutRow = $layoutTable->createRow();
-$versionLayoutRow->setAttribute(0, $versionInfo);
+$versionLayoutRow->setAttribute(0, $lVersion);
 $layoutTable->addRow($versionLayoutRow);
 
 /* --------------------------------- */
