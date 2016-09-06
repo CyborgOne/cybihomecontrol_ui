9271bd536c692adcbcd65ca7a3de3a6c8be13980
Daniel Scheidler <danielscheidler@gmail.com>
2016-09-06 23:24:00 +0200

    mobile_includes bei v0.0.46 vergessen

--- mobile_includes/Startseite.php
+++ mobile_includes/Startseite.php
@@ -1,9 +1,40 @@
 <?PHP
 //Filename: Startseite.php
-$vars = get_class_vars("HomeControlMap");
 
-$hcMap = new HomeControlMap(false, "MOBILE");
+$loginNeed        = getPageConfigParam($_SESSION['config']->DBCONNECT, "loginForSwitchNeed") == "J";
+$loginExternOnly  = getPageConfigParam($_SESSION['config']->DBCONNECT, "loginExternOnly") == "J";
+$loginOK          = ($_SESSION['config']->CURRENTUSER->STATUS == "admin" || $_SESSION['config']->CURRENTUSER->STATUS == "user");
 
-$hcMap->show();
+$clientIP = explode(".", $_SERVER['REMOTE_ADDR']);
+$serverIP = explode(".",$_SERVER['SERVER_ADDR']);
+
+if (!$loginNeed || 
+      $loginOK  ||                   
+      ($loginExternOnly && ($serverIP[0]==$clientIP[0] && $serverIP[1]==$clientIP[1] && $serverIP[2]==$clientIP[2]))
+   ) {
+
+    $hcMap = new HomeControlMap(false, "MOBILE");
+    $hcMap->show();
+            
+} else {
+       
+   /* ------------------------------------
+      BENUTZERSTATUS ANZEIGEN
+    ------------------------------------ */
+    $USR = $_SESSION['config']->CURRENTUSER;
+     
+    $USERSTATUS = new UserStatus($USR, -1, -1);
+    
+    $tbl = new Table( array("") );
+    $tbl->setAlign("center");
+    $r = $tbl->createRow();
+    $r->setAttribute( 0, $USERSTATUS );
+    $tbl->addRow( $r );
+    
+    $tbl->show();
+    /* --------------------------------- */
+
+}
+
 
 ?>
